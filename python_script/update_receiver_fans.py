92bf4d672cbad7cb046bf456851d557349ab422d
Ståle Semb Hauknes <stale.hauknes@gmail.com>
2018-03-30 22:59:20 +0200

    Added python script to control MQTT fans (receiver / tv).

---
+++ python_script/update_receiver_fans.py
@@ -0,0 +1,73 @@
+rear_fan = 'input_number.receiver_fan_speed_rear'
+internal_fan = 'input_number.receiver_fan_speed_internal'
+
+philips_tv = data.get('device_tracker.philips_tv')
+cpu_temp = data.get('sensor.cpu_temp')
+receiver_temp = data.get('sensor.receiver_temperature')
+
+
+def getRearSpeed(cpu, receiver):
+    if philips_tv is not None and philips_tv == False:
+        receiver = 0
+    if c > 75 or receiver > 40:
+        return 100
+    elif c > 76 or receiver > 38:
+        return 90
+    elif c > 74 or receiver > 36:
+        return 80
+    elif c > 72 or receiver > 35:
+        return 70
+    elif c > 70 or receiver > 34:
+        return 60
+    elif c > 65 or receiver > 33:
+        return 50
+    elif c > 60 or receiver > 32:
+        return 40
+    return 0
+
+
+def getInternalSpeed(cpu, receiver):
+    if philips_tv is not None and philips_tv == False:
+        receiver = 0
+    if c > 80 or receiver > 40:
+        return 100
+    elif c > 78 or receiver > 38:
+        return 90
+    elif c > 76 or receiver > 36:
+        return 80
+    elif c > 74 or receiver > 35:
+        return 70
+    elif c > 72 or receiver > 34:
+        return 60
+    elif c > 70 or receiver > 33:
+        return 50
+    elif c > 65 or receiver > 32:
+        return 40
+    return 0
+        
+
+def setSpeed(fan, speed):
+    if speed < 0 || speed > 100:
+        return
+    service_data = {'entity_id': fan, 'value': speed }
+    hass.services.call('input_number', 'set_value', service_data)
+
+
+def setDefaultSpeeds():
+    setSpeed(rear_fan, 20)
+    setSpeed(internal_fan, 20)
+
+
+if cpu_temp is not None and internal_temp is not None:
+    cpu = 0
+    receiver = 0
+    try:
+        cpu = int(cpu_temp)
+        receiver = int(receiver_temp)
+    except:
+        setDefaultSpeeds()
+        return
+    setSpeed(rear_fan, getRearSpeed(cpu, receiver))
+    setSpeed(internal_fan, getRearSpeed(cpu, receiver))
+else:
+    setDefaultSpeeds()
