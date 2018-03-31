3c8580800af82e55d1ada79086b36efe4af295bf
Ståle Semb Hauknes <stale.hauknes@gmail.com>
2018-03-31 19:19:30 +0200

    Updated tv / avr automations and devices. Included a test notification to test if notification is fired.

--- python_scripts/update_receiver_fans.py
+++ python_scripts/update_receiver_fans.py
@@ -2,42 +2,44 @@ rear_fan = 'input_number.receiver_fan_speed_rear'
 internal_fan = 'input_number.receiver_fan_speed_internal'
 
 philips_tv = data.get('philips_tv')
+pioneer_avr = data.get('pioneer_avr')
+
 cpu_temp = data.get('cpu_temp')
 receiver_temp = data.get('receiver_temp')
 
 
 def getRearSpeed(cpu, receiver):
-    if cpu > 75 or receiver > 40:
+    if cpu >= 75 or receiver > 40:
         return 100
-    elif cpu > 76 or receiver > 38:
+    elif cpu >= 76 or receiver > 38:
         return 90
-    elif cpu > 74 or receiver > 36:
+    elif cpu >= 74 or receiver > 36:
         return 80
-    elif cpu > 72 or receiver > 35:
+    elif cpu >= 72 or receiver > 35:
         return 70
-    elif cpu > 70 or receiver > 34:
+    elif cpu >= 70 or receiver > 34:
         return 60
-    elif cpu > 65 or receiver > 33:
+    elif cpu >= 65 or receiver > 33:
         return 50
-    elif cpu > 60 or receiver > 32:
+    elif cpu >= 60 or receiver > 32:
         return 40
     return 0
 
 
 def getInternalSpeed(cpu, receiver):
-    if cpu > 80 or receiver > 40:
+    if cpu >= 80 or receiver >= 42:
         return 100
-    elif cpu > 78 or receiver > 38:
+    elif cpu >= 78 or receiver >= 40:
         return 90
-    elif cpu > 76 or receiver > 36:
+    elif cpu >= 76 or receiver >= 38:
         return 80
-    elif cpu > 74 or receiver > 35:
+    elif cpu >= 74 or receiver >= 37:
         return 70
-    elif cpu > 72 or receiver > 34:
+    elif cpu >= 72 or receiver >= 36:
         return 60
-    elif cpu > 70 or receiver > 33:
+    elif cpu >= 70 or receiver >= 35:
         return 50
-    elif cpu > 65 or receiver > 32:
+    elif cpu >= 65 or receiver >= 34:
         return 40
     return 0
 
@@ -56,7 +58,7 @@ else:
         if cpu_temp is None or receiver_temp is None:
             raise ValueError('Temperatures cannot be "None"')
         cpu = float(cpu_temp)
-        if philips_tv is not None or philips_tv == "not_home":
+        if (philips_tv is None or philips_tv == "not_home") and (pioneer_avr is None or pioneer_avr == "off"):
             receiver = 0
         else:
             receiver = float(receiver_temp)
